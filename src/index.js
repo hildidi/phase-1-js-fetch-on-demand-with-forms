dfa21ae160e9b104f94a7b10ee21c7584d6f4ba4
Hilda Diana Cardenas <hcardenas00@gmail.com>
2021-11-20 19:52:55 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
 const init = () => {
+
+  const formSection = document.querySelector('form');
   
+  formSection.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const input = document.querySelector('input#searchByID')
+
+    
+    fetch(`http://localhost:3000/movies/${input.value}`)
+    .then(r => r.json())
+    .then(moviesArr => {
+        const title = document.querySelector('section#movieDetails h4');
+        const summary = document.querySelector('section#movieDetails p')
+        
+        title.innerText = moviesArr.title;
+        summary.innerText = moviesArr.summary;
+  });
+});
 }
 
 document.addEventListener('DOMContentLoaded', init);
